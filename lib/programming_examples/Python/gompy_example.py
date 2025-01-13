1b702fda4bbb9cdd71ebbdf8791fd33d6a9c64ab
Bruno <Bruno.Levy@inria.fr>
2025-01-13 17:49:57 +0100

    Ready to test gompy in build action ! (created small example)

---
+++ lib/programming_examples/Python/gompy_example.py
@@ -0,0 +1,10 @@
+# To be run outside Graphite (after gompy is installed)
+
+import gompy
+OGF = gom.meta_types.OGF
+
+S = OGF.MeshGrob()
+S.I.Shapes.create_sphere()
+R = S.I.Surface.remesh_smooth()
+R.I.Mesh.display_statistics()
+R.save('remesh.obj')
