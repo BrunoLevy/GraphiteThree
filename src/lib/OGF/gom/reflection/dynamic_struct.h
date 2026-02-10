b35e0a3fcadd62a720bfe06f6f37580d91fbd810
Bruno Levy <Bruno.Levy@inria.fr>
2026-02-10 23:49:08 +0100

    - build.yml: removed '& exit 0' at the end of Windows test (with it I no longer see the command output in the log, it is annoying) - WIP: MetaStruct and Struct types for GOM.

---
+++ src/lib/OGF/gom/reflection/dynamic_struct.h
@@ -0,0 +1,88 @@
+/*
+ *  GXML/Graphite: Geometry and Graphics Programming Library + Utilities
+ *  Copyright (C) 2000 Bruno Levy
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  If you modify this software, you should include a notice giving the
+ *  name of the person performing the modification, the date of modification,
+ *  and the reason for such modification.
+ *
+ *  Contact: Bruno Levy
+ *
+ *     levy@loria.fr
+ *
+ *     ISA Project
+ *     LORIA, INRIA Lorraine,
+ *     Campus Scientifique, BP 239
+ *     54506 VANDOEUVRE LES NANCY CEDEX
+ *     FRANCE
+ *
+ *  Note that the GNU General Public License does not permit incorporating
+ *  the Software into proprietary programs.
+ */
+
+#ifndef H_OGF_META_TYPES_META_STRUCT_H
+#define H_OGF_META_TYPES_META_STRUCT_H
+
+#include <OGF/gom/common/common.h>
+#include <OGF/gom/reflection/meta_class.h>
+
+namespace OGF {
+
+
+    /**
+     * \brief MetaType for structs.
+     */
+    gom_class GOM_API MetaStruct : public MetaClass {
+    public:
+	/**
+         * \brief Constructs a new MetaStruct
+         * \param[in] struct_name the C++ struct name
+	 */
+	MetaStruct(const std::string& struct_name);
+
+        /**
+         * \brief MetaStruct destructor.
+         */
+	~MetaStruct() override;
+
+    gom_slots:
+
+	/**
+	 * \brief Adds a new property to this MetaStruact
+	 * \param[in] property_name the name of the property
+	 * \param[in] property_type a pointer to the MetaType of the property
+	 */
+	MetaProperty* add_property(
+	    const std::string& property_name,
+	    MetaType* property_type
+	);
+
+	/**
+	 * \brief Adds a new property to this MetaStruact
+	 * \param[in] property_name the name of the property
+	 * \param[in] property_type_name the C++ type name of the property
+	 */
+	MetaProperty* add_property(
+	    const std::string& property_name,
+	    const std::string& property_type_name
+	);
+
+    };
+
+}
+
+#endif
