c67b4067d18f4b50455528e0888edfc6b2f628d7
Bruno Levy <Bruno.Levy@inria.fr>
2026-02-20 17:36:32 +0100

    Close button for text editor New geogram version with close button for terminal

--- lib/text_editor.lua
+++ lib/text_editor.lua
@@ -507,7 +507,13 @@ end
 
 function text_editor_gui.draw_window()
    text_editor_gui.parse_errors()
-
+   if imgui.SimpleButton(
+      imgui.font_icon('window-close')..'##close'
+   ) then
+      text_editor_gui.visible = false
+   end
+   autogui.tooltip('Hide editor')
+   imgui.SameLine()
    if imgui.SimpleButton(
       imgui.font_icon('play-circle')
    ) then
