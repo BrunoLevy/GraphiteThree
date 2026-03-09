ea50efe916428e23631122c79031e69c73681b79
Bruno Levy <Bruno.Levy@inria.fr>
2026-03-09 18:25:37 +0100

    WIP: Lua wrapper generator for Dear ImGui:   a bit of code reorg, created a class for the wrapper generator.

--- src/bin/gomgen/generate_luawrap.cpp
+++ src/bin/gomgen/generate_luawrap.cpp
@@ -52,122 +52,234 @@
 namespace {
     using namespace OGF;
 
-    std::vector<MetaType*> integer_types;
 
-    bool type_is_integer_like(MetaType* mtype) {
-	if(dynamic_cast<MetaEnum*>(mtype) != nullptr) {
-	    return true;
+    class LuaWrapGenerator {
+    public:
+	LuaWrapGenerator() {
+	    // List of integer-like types
+	    integer_types_.push_back(ogf_meta<int>::type());
+	    integer_types_.push_back(ogf_meta<long>::type());
+	    integer_types_.push_back(ogf_meta<unsigned int>::type());
+	    integer_types_.push_back(ogf_meta<unsigned long>::type());
+	    integer_types_.push_back(ogf_meta<GEO::index_t>::type());
+	    integer_types_.push_back(ogf_meta<OGF::index_t>::type());
+	    integer_types_.push_back(ogf_meta<size_t>::type());
+
+	    // Pre-declare all integer types, char* and bool* used in ImGui
+
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<signed char>("signed char")
+	    );
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<unsigned char>("unsigned char")
+	    );
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<short>("short")
+	    );
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<unsigned short>("unsigned short")
+	    );
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<long long>("long long")
+	    );
+	    integer_types_.push_back(
+		ogf_declare_builtin_type<unsigned long long>(
+		    "unsigned long long"
+		)
+	    );
+
+	    ogf_declare_pointer_type<char*>("char*");
+	    ogf_declare_pointer_type<bool*>("bool*");
+	    ogf_declare_pointer_type<int*>("int*");
+	    ogf_declare_pointer_type<float*>("float*");
+	    ogf_declare_pointer_type<double*>("double*");
 	}
 
-	if(
-	    std::find(integer_types.begin(), integer_types.end(), mtype) !=
-	    integer_types.end()
-	) {
-	    return true;
-	}
 
-	return false;
-    }
-
-    bool type_is_integer_like(const std::string& type_name) {
-	MetaType* mtype = Meta::instance()->resolve_meta_type(type_name);
-	return type_is_integer_like(mtype);
-    }
-
-    bool check_type(const std::string& type_name) {
-	if(type_name == "ImVec2" || type_name == "ImVec4") {
-	    return true;
-	}
-	if(type_name == "void*") {
-	    return false;
-	}
-	MetaType* mtype = Meta::instance()->resolve_meta_type(type_name);
-	if(mtype == nullptr) {
-	    return false;
-	}
-	if(dynamic_cast<MetaClass*>(mtype) != nullptr) {
-	    return false;
-	}
-	return true;
-    }
-
-    bool check_types(MetaMethod* mmethod, bool report=false) {
-	bool OK = true;
-	for(index_t i=0; i<mmethod->nb_args(); ++i) {
-	    MetaArg* marg = mmethod->ith_arg(i);
-	    if(report) {
-		Logger::out("GomGen")
-		    << "   arg: " << marg->name()
-		    << ":" << marg->type_name()
-		    << " " << (check_type(marg->type_name()) ? "OK" : "KO")
-		    << std::endl;
+	/**
+	 * \brief Tests whether a MetaType corresponds to an integer type
+	 * \param[in] mtype a pointer to the MetaType
+	 * \retval true if mtype corresponds to a (signed or not) integer of
+	 *  any bitlength
+	 * \retval false otherwise
+	 */
+	bool type_is_integer_like(MetaType* mtype) const {
+	    if(dynamic_cast<MetaEnum*>(mtype) != nullptr) {
+		return true;
 	    }
-	    OK = OK && check_type(marg->type_name());
+	    return(
+		std::find(integer_types_.begin(), integer_types_.end(), mtype) !=
+		integer_types_.end()
+	    );
 	}
-	if(mmethod->return_type_name() != "void") {
-	    if(report) {
-		Logger::out("GomGen")
-		    << "   ret type: " << mmethod->return_type_name()
-		    << " " << (
-			check_type(mmethod->return_type_name()) ? "OK" : "KO"
-		    )
-		    << std::endl;
+
+	/**
+	 * \brief Tests whether a MetaType corresponds to an integer type
+	 * \param[in] type_name a string with the type name, for instance
+	 *  "unsigned int", "signed char", "long long int".
+	 * \retval true if mtype corresponds to a (signed or not) integer of
+	 *  any bitlength
+	 * \retval false otherwise
+	 */
+	bool type_is_integer_like(const std::string& type_name) const {
+	    MetaType* mtype = Meta::instance()->resolve_meta_type(type_name);
+	    return type_is_integer_like(mtype);
+	}
+
+	/**
+	 * \brief Tests whether a type is supported by the Lua wrapping mechanism
+	 * \retval true if the type is supported
+	 * \retval false otherwise, then no wrapper can be generated
+	 *  for a function if the function uses this type as an argument
+	 *  or as its return type
+	 */
+	bool check_type(const std::string& type_name) const {
+	    if(type_name == "ImVec2" || type_name == "ImVec4") {
+		return true;
 	    }
-	    OK = OK && check_type(mmethod->return_type_name());
+	    if(type_name == "void*") {
+		return false;
+	    }
+	    MetaType* mtype = Meta::instance()->resolve_meta_type(type_name);
+	    if(mtype == nullptr) {
+		return false;
+	    }
+	    if(dynamic_cast<MetaClass*>(mtype) != nullptr) {
+		return false;
+	    }
+	    return true;
 	}
-	return OK;
-    }
 
-    std::string show_type_name(const std::string& type_name) {
-	if(type_is_integer_like(type_name)) {
-	    return type_name + "(I)";
-	}
-	return type_name;
-    }
 
-    void show_imgui() {
-	MetaClass* mclass = Meta::instance()->resolve_meta_class("ImGui");
-	if(mclass == nullptr) {
-	    std::cerr << "  DID NOT FIND ImGui" << std::endl;
-	    return;
+	/**
+	 * \brief Tests whether a wrapper can be generated for a function
+	 * \param[in] mmethod the MetaMethod that corresponds to the function
+	 * \param[in] report if set, report all parameters, return type, and
+	 *  whether they can be handled by a wrapper
+	 * \retval true if a wrapper can be generated for a method
+	 * \retval false otherwise
+	 */
+	bool check_types(MetaMethod* mmethod, bool report=false) {
+	    bool OK = true;
+	    for(index_t i=0; i<mmethod->nb_args(); ++i) {
+		MetaArg* marg = mmethod->ith_arg(i);
+		if(report) {
+		    Logger::out("GomGen")
+			<< "   arg: " << marg->name()
+			<< ":" << marg->type_name()
+			<< " " << (check_type(marg->type_name()) ? "OK" : "KO")
+			<< std::endl;
+		}
+		OK = OK && check_type(marg->type_name());
+	    }
+	    if(mmethod->return_type_name() != "void") {
+		if(report) {
+		    Logger::out("GomGen")
+			<< "   ret type: " << mmethod->return_type_name()
+			<< " " << (
+			    check_type(mmethod->return_type_name()) ? "OK" : "KO"
+			)
+			<< std::endl;
+		}
+		OK = OK && check_type(mmethod->return_type_name());
+	    }
+	    if(OK) {
+		for(index_t i=0; i<mmethod->nb_args(); ++i) {
+		    MetaArg* marg = mmethod->ith_arg(i);
+		    MetaType* mtype = Meta::instance()->resolve_meta_type(
+			marg->type_name()
+		    );
+		    used_types_.insert(mtype);
+		}
+		if(mmethod->return_type_name() != "void") {
+		    MetaType* mtype = Meta::instance()->resolve_meta_type(
+			mmethod->return_type_name()
+		    );
+		    used_types_.insert(mtype);
+		}
+	    }
+	    return OK;
 	}
-	index_t N = index_t(mclass->nb_members());
-	for(index_t i=0; i<N; ++i) {
-	    MetaMember* mmember = mclass->ith_member(i);
-	    MetaMethod* mmethod = dynamic_cast<MetaMethod*>(mmember);
-	    if(mmethod != nullptr) {
-		bool OK = check_types(mmethod);
-		std::string proto;
-		if(true) {
-		    proto += (show_type_name(mmethod->return_type_name()) + " ");
-		    proto += (mmethod->name() + "(");
-		    for(index_t i=0; i<mmethod->nb_args(); ++i) {
-			MetaArg* marg = mmethod->ith_arg(i);
+
+	/**
+	 * \brief decorates a type name with additional information
+	 * \param[in] type_name a type name
+	 * \return type_name plus some additional information:
+	 *   - "(I)" for integer-like or enum types
+	 */
+	std::string show_type_name(const std::string& type_name) const {
+	    if(type_is_integer_like(type_name)) {
+		return type_name + "(I)";
+	    }
+	    return type_name;
+	}
+
+
+	/**
+	 * \brief Generates wrappers for all functions in a namespace
+	 * \param[in] name_space the name of the namespace, as a string
+	 */
+	void generate_wrappers(const std::string& name_space) {
+	    MetaClass* mclass = Meta::instance()->resolve_meta_class(name_space);
+	    if(mclass == nullptr) {
+		Logger::err("Gom::CodeGen") << "Did not find namespace:"
+					    << name_space
+					    << std::endl;
+		return;
+	    }
+	    index_t N = index_t(mclass->nb_members());
+	    for(index_t i=0; i<N; ++i) {
+		MetaMember* mmember = mclass->ith_member(i);
+		MetaMethod* mmethod = dynamic_cast<MetaMethod*>(mmember);
+		if(mmethod != nullptr) {
+		    bool OK = check_types(mmethod);
+		    std::string proto;
+		    if(true) {
 			proto += (
-			    show_type_name(marg->type_name()) + " " +
-			    marg->name()
+			    show_type_name(mmethod->return_type_name()) + " "
 			);
-			if(marg->has_default_value()) {
-			    proto += ("=" + marg->default_value().as_string());
+			proto += (mmethod->name() + "(");
+			for(index_t i=0; i<mmethod->nb_args(); ++i) {
+			    MetaArg* marg = mmethod->ith_arg(i);
+			    proto += (
+				show_type_name(marg->type_name()) + " " +
+				marg->name()
+			    );
+			    if(marg->has_default_value()) {
+				proto += ("=" +
+					  marg->default_value().as_string()
+					 );
+			    }
+			    if(i != mmethod->nb_args()-1) {
+				proto += ",";
+			    }
 			}
-			if(i != mmethod->nb_args()-1) {
-			    proto += ",";
-			}
+			proto += ")";
+		    } else {
+			proto = mmethod->name();
 		    }
-		    proto += ")";
-		} else {
-		    proto = mmethod->name();
-		}
-		Logger::out("GomGen")
-		    <<  (OK ? "OK " : "KO ")
-		    << proto
-		    << std::endl;
-		if(!OK) {
-		    check_types(mmethod,true);
+		    Logger::out("GomGen") << (OK ? "OK " : "KO ") << proto
+					  << std::endl;
+		    if(!OK) {
+			check_types(mmethod,true);
+		    }
 		}
 	    }
 	}
-    }
+
+    private:
+	/**
+	 * \brief all integer-like types, signed and unsigned, with various
+	 * bitlength
+	 */
+	std::vector<MetaType*> integer_types_;
+
+	/**
+	 * \brief all the types used as argument or return types by the
+	 *  generated functions
+	 */
+	std::set<MetaType*> used_types_;
+    };
 }
 
 /****************************************************************************/
@@ -186,42 +298,7 @@ void generate_luawrap(
     Swig_process_types(top);
     Swig_default_allocators(top);
 
-    // List of integer-like types
-
-    integer_types.push_back(ogf_meta<int>::type());
-    integer_types.push_back(ogf_meta<long>::type());
-    integer_types.push_back(ogf_meta<unsigned int>::type());
-    integer_types.push_back(ogf_meta<unsigned long>::type());
-    integer_types.push_back(ogf_meta<GEO::index_t>::type());
-    integer_types.push_back(ogf_meta<OGF::index_t>::type());
-    integer_types.push_back(ogf_meta<size_t>::type());
-
-    // Pre-declare all integer types, char* and bool* used in ImGui
-
-    integer_types.push_back(
-	ogf_declare_builtin_type<signed char>("signed char")
-    );
-    integer_types.push_back(
-	ogf_declare_builtin_type<unsigned char>("unsigned char")
-    );
-    integer_types.push_back(
-	ogf_declare_builtin_type<short>("short")
-    );
-    integer_types.push_back(
-	ogf_declare_builtin_type<unsigned short>("unsigned short")
-    );
-    integer_types.push_back(
-	ogf_declare_builtin_type<long long>("long long")
-    );
-    integer_types.push_back(
-	ogf_declare_builtin_type<unsigned long long>("unsigned long long")
-    );
-
-    ogf_declare_pointer_type<char*>("char*");
-    ogf_declare_pointer_type<bool*>("bool*");
-    ogf_declare_pointer_type<int*>("int*");
-    ogf_declare_pointer_type<float*>("float*");
-    ogf_declare_pointer_type<double*>("double*");
+    LuaWrapGenerator generator;
 
     if (top) {
 	if (Swig_contract_mode_get()) {
@@ -229,6 +306,6 @@ void generate_luawrap(
 	}
 	lang->top(top);
 
-	show_imgui();
+	generator.generate_wrappers("ImGui");
     }
 }
